54f3a8d5eeae5a90d523e0672671cab29c03d678
DDorka02 <dobszaydorka@gmail.com>
2024-12-10 09:48:05 +0100

    jav

--- src/componens/Lampa.js
+++ src/componens/Lampa.js
@@ -2,9 +2,12 @@ import React from 'react'
 import "./Lampa.css"
 
 export default function Lampa(props) {
+    function katt(){
+        props.katt(props.index)
+    }
   return (
-    <div className="lampa"> 
-        O
+    <div className={`cella ${props.igaz?"fel":"le"}`}> 
+        
     </div>
   )
 }
